725176f581452cedc34db3e2aabd48d9fc866a4d
phlx0 <234970933+phlx0@users.noreply.github.com>
2026-04-26 23:21:20 +0200

    chore: add license, gitignore, and web-ext config

---
+++ .web-ext-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ignoreFiles: [
+    ".github/**",
+    ".gitignore",
+    ".web-ext-config.js",
+    "LICENSE",
+    "CHANGELOG.md",
+    "README.md",
+    "dist/**",
+    "web-ext-artifacts/**",
+  ],
+};
